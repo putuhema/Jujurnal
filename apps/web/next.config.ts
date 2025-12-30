b6d3f7b9019cdda383347544a2fee1ea08c88e3d
putu hendra mahendra <putuhendramahendra@gmail.com>
2025-12-30 12:41:32 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@puma-brain/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ apps/web/src/lib/auth-server.ts
@@ -0,0 +1,17 @@
+import { convexBetterAuthNextJs } from "@convex-dev/better-auth/nextjs";
+import { env } from "@puma-brain/env/web";
+
+import { isAuthError } from "@/lib/utils";
+
+export const {
+  handler,
+  preloadAuthQuery,
+  isAuthenticated,
+  getToken,
+  fetchAuthQuery,
+  fetchAuthMutation,
+  fetchAuthAction,
+} = convexBetterAuthNextJs({
+  convexUrl: env.NEXT_PUBLIC_CONVEX_URL,
+  convexSiteUrl: env.NEXT_PUBLIC_CONVEX_SITE_URL,
+});
